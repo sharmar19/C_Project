09cb6c38307afcf1da4f10cc0838bfc7f005ccce
Rudraksh Sharma <sharmar19@outlook.com>
2025-11-25 13:39:53 +0530

    .c files

--- src/main.c
+++ src/main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include "../include/library.h" //include folder
+#include <stdlib.h>
+#include "../include/library.h" 
 
 int main() {
     int choice;
+    int searchId;
+
     while(1) {
-        printf("\n=== Library Management System ===\n");
+        printf("\nLibrary Management System:\n");
         printf("1. Add Book\n");
         printf("2. List Books\n");
-        printf("3. Add Member\n");
-        printf("4. Exit\n");
+        printf("3. Search Book by ID\n"); 
+        printf("4. Add Member\n");
+        printf("5. List Members\n");   
+        printf("6. Exit\n");
         printf("Enter choice: ");
         
         if (scanf("%d", &choice) != 1) {
-            printf("Invalid input. Please enter a number.\n"); //invalid input handling
+            printf("Invalid input. Please enter a number.\n"); 
             while(getchar() != '\n'); 
             continue;
         }
@@ -25,13 +30,25 @@ int main() {
                 list_books();
                 break;
             case 3:
-                add_member();
+                printf("Enter Book ID to search: ");
+                if (scanf("%d", &searchId) != 1) {
+                    printf("Invalid Input.\n");
+                    while(getchar() != '\n');
+                } else {
+                    search_book_by_id(searchId);
+                }
                 break;
             case 4:
+                add_member();
+                break;
+            case 5:
+                list_members(); 
+                break;
+            case 6:
                 printf("Exiting system...\n");
                 return 0;
             default:
-                printf("Invalid choice! Try again.\n"); //invalid choice handling
+                printf("Invalid choice! Try again.\n");
         }
     }
     return 0;
